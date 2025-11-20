763871aed10c821dfe17c25d29306617dedfcd04
marcocarri <carricatomarco@gmail.com>
2025-11-20 17:31:26 +0100

    V 0.30 ampliate la classe ToDoList ed implementate nuove funzionalità

--- inc/ToDoList.h
+++ inc/ToDoList.h
@@ -1,4 +1,25 @@
 #ifndef CARRICATO_TODOLIST_TODOLIST_H
 #define CARRICATO_TODOLIST_TODOLIST_H
 
+#include "Task.h"
+#include <vector>
+#include <string>
+
+class ToDoList {
+private:
+    std::vector<Task> tasks;
+    const std::string filename="carricato_listaToDo.txt";
+
+public:
+    ToDoList();
+    ~ToDoList();
+
+    const std::vector<Task>& getTasks() const;
+    const std::string& getFileName() const;
+    void setTasks(const std::vector<Task>& tasks);
+
+    void saveToFile();
+    void loadFromFile();
+};
+
 #endif //CARRICATO_TODOLIST_TODOLIST_H
--- src/ToDoList.cpp
+++ src/ToDoList.cpp
@@ -1,1 +1,21 @@
 #include "../inc/ToDoList.h"
+
+using namespace std;
+
+ToDoList::ToDoList() {
+    loadFromFile();
+}
+ToDoList::~ToDoList() {
+    saveToFile();
+}
+
+const std::vector<Task>& ToDoList::getTasks() const {
+    return this->tasks;
+};
+const std::string& ToDoList::getFileName() const {
+    return this->filename;
+}
+
+void ToDoList::setTasks(const std::vector<Task>& tasks) {
+    this->tasks=tasks;
+}
