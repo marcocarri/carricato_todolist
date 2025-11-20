828adf1e7c508d081019779920f815e57c720800
marcocarri <carricatomarco@gmail.com>
2025-11-20 17:20:42 +0100

    V 0.24 ampliate le funzionalità di Task e completata la sua implementazione. Task ora descrive un appuntamento, ed è composto da una data ed un orario, un luogo, una descrizione e una variabile che indica se l'impegno è stato completato o meno. La classe permette di creare oggetti; la stampa a stringa con formato fisso e ottenere e settari i suoi valori

--- inc/Task.h
+++ inc/Task.h
@@ -1,22 +1,30 @@
 #ifndef CARRICATO_TODOLIST_TASK_H
 #define CARRICATO_TODOLIST_TASK_H
 
+#include "DateTime.h"
+#include "Location.h"
 #include <string>
 
 class Task {
 private:
     std::string description;
     bool completed;
+    DateTime date_time;
+    Location location;
 
 public:
 
-    Task(const std::string& description);
+    Task(const std::string& description,const DateTime& date_time,const Location& location);
     std::string toString() const;
     const std::string& getDescription() const;
     bool isCompleted() const;
+    const DateTime& getDateTime() const;
+    const Location& getLocation() const;
     void setDescription(const std::string& description);
     void markCompleted();
     void markPending();
+    void setDateTime(const DateTime& date_time);
+    void setLocation(const Location& location);
 
     void save();
     void load();
--- src/Task.cpp
+++ src/Task.cpp
@@ -1,11 +1,11 @@
 #include "../inc/Task.h"
 #include <sstream>
 
-Task::Task(const std::string &description) : description(description) {}
+Task::Task(const std::string& description,const DateTime& date_time,const Location& location) : description(description),date_time(date_time),location(location) {}
 
-std::string Task::toString() const {
+std::string Task::toString() const { //stampo la task nel formato --> Date & Time: GG-MM-AAAA HH:MM:SS; Location: location; Completed: 1/0; Description: description
     std::ostringstream toString;
-    toString << "Completed: ";
+    toString << "Date & Time: " << this->date_time.toString() << "; Location: " << this->location.getAddress() << "; Completed: ";
     if (this->completed) toString << "yes; Description: " << this->description;
     else toString << "no; Description: " << this->description;
     return toString.str();
@@ -17,6 +17,12 @@ const std::string& Task::getDescription() const {
 bool Task::isCompleted() const {
     return this->completed;
 }
+const DateTime& Task::getDateTime() const {
+    return this->date_time;
+}
+const Location& Task::getLocation() const {
+    return this->location;
+}
 void Task::setDescription(const std::string& description) {
     this->description=description;
 }
@@ -25,4 +31,10 @@ void Task::markCompleted() {
 }
 void Task::markPending() {
     this->completed=false;
+}
+void Task::setDateTime(const DateTime &date_time) {
+    this->date_time=date_time;
+}
+void Task::setLocation(const Location& location) {
+    this->location=location;
 }
